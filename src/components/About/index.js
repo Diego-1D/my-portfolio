34cc6a20e3aa5803011c1013d54d1c0e3e4d7ba1
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-09 23:22:08 -0300

    Criação do componente About

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container,
+} from './styles';
+
+const About = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default About;
---
+++ src/components/About/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    height: 100vh;
+    padding: 0 5rem;
+    display: flex;
+    background: #161E35;
+`;
