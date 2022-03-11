f37cd8d82c40a93476b6c6bec1451221c62a09b6
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-11 13:01:20 -0300

    Criação do Data

---
+++ src/data/data.js
@@ -0,0 +1,135 @@
+//IMAGES
+import LinkenInImage from '../Images/linkedIn.png';
+import AnimeImage from '../Images/anime.png';
+import TheCocktailImage from '../Images/theCocktail.png';
+import FinancesControlImage from '../Images/financesControl.png';
+import IceCreamImage from '../Images/iceCream.png';
+import FitSpaImage from '../Images/fitSpa.png';
+
+//ICONS
+import FirebaseIcon from '../Images/Icons/firebase.svg';
+import ReactIcon from '../Images/Icons/react.svg';
+import HmtlIcon from '../Images/Icons/html.svg';
+import CssIcon from '../Images/Icons/css.svg';
+import JavaScriptIcon from '../Images/Icons/javascript.svg';
+import AntDesignIcon from '../Images/Icons/antDesign.svg';
+import StyledComponentsIcon from '../Images/Icons/styledComponents.svg';
+import SassIcon from '../Images/Icons/sass.svg';
+import TypeScriptIcon from '../Images/Icons/typescript.svg';
+
+export const Projects = [
+    {
+        key: 1,
+        image: LinkenInImage,
+        name: 'Clone do LinkedIn',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+    {
+        key: 2,
+        image: AnimeImage,
+        name: 'Anime',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+    {
+        key: 3,
+        image: TheCocktailImage,
+        name: 'The Cocktail',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+    {
+        key: 4,
+        image: FinancesControlImage,
+        name: 'Finances Control',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+    {
+        key: 5,
+        image: IceCreamImage,
+        name: 'Menu Ice Cream ',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+    {
+        key: 6,
+        image: FitSpaImage,
+        name: 'Fit Spa',
+        tecnoUsed: [
+            FirebaseIcon,
+            ReactIcon,
+            HmtlIcon,
+            CssIcon
+        ]
+    },
+];
+
+export const Technologies = [
+    {
+        key: 1,
+        icon: JavaScriptIcon,
+        name: 'JavaScript'
+    },
+    {
+        key: 2,
+        icon: HmtlIcon,
+        name: 'Html'
+    },
+    {
+        key: 3,
+        icon: ReactIcon,
+        name: 'React'
+    },
+    {
+        key: 4,
+        icon: TypeScriptIcon,
+        name: 'TypeScript'
+    },
+    {
+        key: 5,
+        icon: CssIcon,
+        name: 'Css'
+    },
+    {
+        key: 6,
+        icon: StyledComponentsIcon,
+        name: 'Styled Components'
+    },
+    {
+        key: 7,
+        icon: SassIcon,
+        name: 'Sass'
+    },
+    {
+        key: 8,
+        icon: AntDesignIcon,
+        name: 'Ant Design'
+    },
+    {
+        key: 9,
+        icon: FirebaseIcon,
+        name: 'Firebase'
+    },
+];
