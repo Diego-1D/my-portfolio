e1a8af5b20d922740f5cc5d335e2608848b1be69
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-11 12:56:45 -0300

    Criação do componente Skills

---
+++ src/components/Skills/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Technologies } from '../../data/data';
+import {
+    Container,
+    Title,
+    Wrapper,
+    TechnologiesIcon,
+    AreaIcon,
+    TitleIcon
+} from './styles';
+
+const Skills = () => {
+    return (
+        <Container>
+            <Title>Software Skills</Title>
+            <Wrapper>
+                {
+                    Technologies.map((index, key) => {
+                        return (
+                            <AreaIcon key={key}>
+                                <TechnologiesIcon src={index.icon} />
+                                <TitleIcon>{index.name}</TitleIcon>
+                            </AreaIcon>
+                        )
+                    })
+                }
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Skills;
---
+++ src/components/Skills/styles.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    height: 100vh;
+    padding: 1.5rem 5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #161E35;
+    letter-spacing: 1px;
+    gap: 4rem;
+    
+`;
+
+export const Title = styled.h3`
+    font-size: 24px;
+    font-weight: bold;
+    color: #E6E7F4;
+    text-decoration: underline;
+    text-decoration-color: #F8032D;
+`;
+
+export const Wrapper = styled.div`
+    width: 55%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+`;
+
+export const AreaIcon = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`;
+
+export const TechnologiesIcon = styled.img`
+    width: 60px;
+    height: 60px;
+    margin-bottom: 5px;
+    /* background-color: yellow; */
+`;
+
+export const TitleIcon = styled.h5`
+    font-size: 16px;
+    font-weight: bold;
+    color: #E6E7F4;
+    letter-spacing: 1px;
+`;
