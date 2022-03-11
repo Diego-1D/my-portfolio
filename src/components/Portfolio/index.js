f90171dcce329741a3b95dfc044f964c13a1d53c
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-11 12:58:42 -0300

    Alteração no componente Portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Projects } from '../../data/data';
 import {
   Container,
   Title,
   Wrapper,
-  Card
+  Card,
+  Imagem,
+  Name,
+  TechnologiesUsedArea,
+  TechnologiesUsed
 } from './styles';
 
 const Portfolio = () => {
@@ -11,24 +16,22 @@ const Portfolio = () => {
     <Container>
       <Title>Portfólio</Title>
       <Wrapper>
-        <Card>
-
-        </Card>
-        <Card>
-
-        </Card>
-        <Card>
-
-        </Card>
-        <Card>
-
-        </Card>
-        <Card>
-
-        </Card>
-        <Card>
-
-        </Card>
+        {
+          Projects.map((index, key) => {
+            return (
+              <Card key={key}>
+                <Imagem src={index.image} />
+                <Name>{index.name}</Name>
+                {/* <TechnologiesUsedArea>
+                  <TechnologiesUsed src={IconeI} />
+                  <TechnologiesUsed src={IconeII} />
+                  <TechnologiesUsed src={IconeIII} />
+                  <TechnologiesUsed src={IconeIIII} />
+                </TechnologiesUsedArea> */}
+              </Card>
+            )
+          })
+        }
       </Wrapper>
     </Container>
   )
