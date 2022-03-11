7f98f1e25abf330aa348d84ff8c466da0e5874ff
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-11 13:06:22 -0300

    Estilização dos componentes

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -4,5 +4,40 @@ export const Container = styled.section`
     height: 100vh;
     padding: 0 5rem;
     display: flex;
+    align-items: center;
     background: #161E35;
+    gap: 5rem;
+`;
+
+export const ImageArea = styled.div``;
+
+export const ImageProfile = styled.img`
+    width: 350px;
+    object-fit: cover;
+`;
+
+export const InfoArea = styled.div`
+    letter-spacing: 1px;
+`;
+
+export const Title = styled.h3`
+    font-size: 24px;
+    font-weight: bold;
+    color: #E6E7F4;
+    text-decoration: underline;
+    text-decoration-color: #F8032D;
+    line-height: 2.5rem;
+`;
+
+export const SubTitle = styled.h5`
+    font-size: 16px;
+    font-weight: 300;
+    color: #E6E7F4;
+    line-height: 2.5rem;
+`;
+
+export const Info = styled.span`
+    width: 44rem;
+    font-size: 16px;
+    color: #ACABBD;
 `;
--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-width: 100%;
+    width: 100%;
     height: 100vh;
-    padding: 0 5rem;
+    padding: 1.5rem 5rem;
     display: flex;
     flex-direction: column;
     background: #161E35;
     letter-spacing: 1px;
+    gap: 1rem;
 `;
 
 export const Title = styled.h3`
@@ -23,12 +24,34 @@ export const Wrapper = styled.div`
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
-    background-color: cyan;
     gap: 3rem;
 `;
 
 export const Card = styled.div`
-    width: 250px;
-    height: 250px;
-    background-color: yellow;
+    width: 300px;
+    height: 260px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Imagem =  styled.img`
+    width: 100%;
+    height: 200px;
+    border-radius: 3px;
+`;
+
+export const Name = styled.h5`
+    font-size: 20px;
+    font-weight: 300;
+    color: #ACABBD;
+`;
+
+export const TechnologiesUsedArea = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
+
+export const TechnologiesUsed = styled.img`
+    width: 15px;
+    object-fit: cover;
 `;
