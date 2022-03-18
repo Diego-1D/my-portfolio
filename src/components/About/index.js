c91f4b501ab215a3ef4e39d28468fbf9ce19ece1
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-18 12:56:32 -0300

    Pequenas alterações

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ImgProfile from '../../Images/profileII.png';
 import {
   Container,
-  ImageArea,
+  Wrapper,
   ImageProfile,
   InfoArea,
   Title,
   Info,
+  AreaImage,
   InfoSubTitle,
   SubTitle
 } from './styles';
@@ -14,27 +15,29 @@ import {
 const About = () => {
   return (
     <Container id='about'>
-      <ImageArea>
-        <ImageProfile src={ImgProfile}></ImageProfile>
-      </ImageArea>
-      <InfoArea>
-        <Title>Sobre mim</Title>
-        <Info>
-          Profissional formado na área de Sistemas de Informação, com experiência na área de desenvolvimento de Front-End para aplicações Web.
-          Atualmente, possuo pequenos projetos realizados com desenvolvimento utilizando principalmente: ReactJs, Firebase, Frameworks para CSS,
-          como Styled-Components e Ant Design, além do próprio CSS.<br />
-          Como desenvolvedor, sempre busco novas formas e métodos para criações de interfaces, não se limitando as tecnologias, possuindo
-          facilidade de aprender novas tecnologias.
-        </Info>
-        <InfoSubTitle>
-          <SubTitle>Nome: <Info>Diego Fernandes Andrade</Info></SubTitle>
-          <SubTitle>Idade: <Info>23 anos</Info></SubTitle>
-          <SubTitle>Contato: <Info>(93) 99231-5428 / (93) 98400-0895</Info></SubTitle>
-          <SubTitle>E-mail: <Info>diego1d_fernandes@outlook.com</Info></SubTitle>
-          <SubTitle>LinkedIn: <Info>https://www.linkedin.com/in/diego-fernandes-dev</Info></SubTitle>
-          <SubTitle>GitHub: <Info>https://github.com/Diego-1D</Info></SubTitle>
-        </InfoSubTitle>
-      </InfoArea>
+      <Wrapper>
+        <AreaImage>
+          <ImageProfile src={ImgProfile} />
+        </AreaImage>
+        <InfoArea>
+          <Title>Sobre mim</Title>
+          <Info>
+            Profissional formado na área de Sistemas de Informação, com experiência na área de desenvolvimento de Front-End para aplicações Web.
+            Atualmente, possuo pequenos projetos realizados com desenvolvimento utilizando principalmente: ReactJs, Firebase, Frameworks para CSS,
+            como Styled-Components e Ant Design, além do próprio CSS.<br />
+            Como desenvolvedor, sempre busco novas formas e métodos para criações de interfaces, não se limitando as tecnologias, possuindo
+            facilidade de aprender novas tecnologias.
+          </Info>
+          <InfoSubTitle>
+            <SubTitle>Nome: <Info>Diego Fernandes Andrade</Info></SubTitle>
+            <SubTitle>Idade: <Info>23 anos</Info></SubTitle>
+            <SubTitle>Contato: <Info>(93) 99231-5428 / (93) 98400-0895</Info></SubTitle>
+            <SubTitle>E-mail: <Info>diego1d_fernandes@outlook.com</Info></SubTitle>
+            <SubTitle>LinkedIn: <Info>https://www.linkedin.com/in/diego-fernandes-dev</Info></SubTitle>
+            <SubTitle>GitHub: <Info>https://github.com/Diego-1D</Info></SubTitle>
+          </InfoSubTitle>
+        </InfoArea>
+      </Wrapper>
     </Container>
   )
 }
--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+    width: 100%;
     height: 100vh;
     padding: 1.5rem 5rem;
     display: flex;
