1e452f2a48118dd709b1f91bc6c780c38b20e1b1
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-18 12:57:51 -0300

    Chamada ao menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-scroll';
+import Sidebar from "../Sidebar";
 import {
   Container,
   Name,
   Header,
-  HeaderOption
+  HeaderOption,
+  OpenMenu,
+  CloseMenu
 } from "./styles";
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(false);
+
+  function toggle() {
+    setIsOpen(!isOpen);
+    setSelected(!isOpen);
+  }
+
   return (
     <Container>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <Name>Diego <span style={{ color: '#F8032D' }}>Fernandes</span></Name>
+      {!selected ?
+        <OpenMenu onClick={toggle} />
+        :
+        <CloseMenu onClick={toggle} />
+      }
       <Header>
         <Link to='hero' smooth={true} duration={1000}>
           <HeaderOption>Home</HeaderOption>
