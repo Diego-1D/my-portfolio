cdd6a19cc6bb94078c330bcd97c7da5069c9045e
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-18 12:54:47 -0300

    Estilo Global

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background: #161E35;
+    font-family: 'Poppins', sans-serif;
+}
+`;
