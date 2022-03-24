6ae68e6856d27f4af7bba7fa8a89af4d73fbc314
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-24 13:57:37 -0300

    Modo responsivo do componente Hero

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -2,32 +2,45 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
-    height: 100vh;
+    height: auto;
     display: flex;
-    align-items: center;
-    padding: 0 5rem;
+    justify-content: center;
+    padding-top: 6rem;
     color: #ACABBD;
 `;
 
+export const Wrapper = styled.div`
+    width: 85%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+`;
+
 export const InfoArea = styled.div`
+    flex: 1;
     letter-spacing: 1px;
 `;
 
-export const Text = styled.p`
-    font-size: 30px;
+export const Text = styled.label`
+    font-size: 2em;
     font-weight: bold;
-    line-height: 3.2rem;
+    line-height: 3rem;
 `;
 
-export const TextSpan = styled.span`
+export const TextSpan = styled.strong`
     color: #E6E7F4;
-    font-size: 42px;
+    font-size: 1.2em;
 `;
 
 export const Description = styled.p`
-    font-size: 14px;
+    font-size: 1em;
     width: 35rem;
-    margin: 1rem 0;
+    margin: 1.2rem 0;
+
+    @media screen and (max-width: 1100px){
+        width: 100%;
+    }
 `;
 
 export const HireButton = styled.button`
@@ -36,19 +49,24 @@ export const HireButton = styled.button`
     border: none;
     border-radius: 20px;
     background: #A41831;
-    font-size: 0.895rem;
+    font-size: 0.895em;
     font-weight: bold;
     color: #E6E7F4;
     letter-spacing: 1px;
 `;
 
 export const ImageArea = styled.div`
-    flex: 1;
+    width: 400px;
     display: flex;
     justify-content: center;
+    margin: 2rem 0;
 `;
 
 export const ImageProfile = styled.img`
     width: 350px;
     object-fit: cover;
+
+    @media screen and (max-width: 1100px){
+        width: 250px;
+    }
 `;
