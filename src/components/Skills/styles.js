d977865341ed0025c29e148d55bd1f5c5b4dfdad
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-03-24 13:54:58 -0300

    Modo responsivo do componente Skills

--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
     width: 100%;
-    height: 100vh;
-    padding: 1.5rem 5rem;
+    min-height: 100vh;
+    height: auto;
+    padding: 4.5rem 3rem;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
     letter-spacing: 1px;
-    gap: 4rem;
-    
+    gap: 3rem;
+    background: #161E35;
 `;
 
 export const Title = styled.h3`
-    font-size: 24px;
+    font-size: 1.8em;
     font-weight: bold;
     color: #E6E7F4;
     text-decoration: underline;
@@ -22,30 +22,34 @@ export const Title = styled.h3`
 `;
 
 export const Wrapper = styled.div`
-    width: 55%;
+    width: 650px;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
     gap: 5rem;
+
+    @media screen and (max-width: 669px){
+        width: 100%;
+        flex-direction: column;
+        gap: 3rem;
+    }
 `;
 
 export const AreaIcon = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-
 `;
 
 export const TechnologiesIcon = styled.img`
-    width: 60px;
-    height: 60px;
+    width: 4em;
+    height: 4em;
     margin-bottom: 5px;
-    /* background-color: yellow; */
 `;
 
 export const TitleIcon = styled.h5`
-    font-size: 16px;
+    font-size: 1em;
     font-weight: bold;
     color: #E6E7F4;
     letter-spacing: 1px;
